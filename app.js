e476bf31d0741576c1a9130f2f70959732ef6fe9
George Codat Tudoran <office@solcreation.ro>
2016-08-02 19:05:48 +0300

    Added html design, ejs template and routes

--- app.js
+++ app.js
@@ -2,8 +2,43 @@ var express = require('express');
 
 var app = express();
 
-var port = 3000;
+var port = process.env.PORT || 3000;
+var bookRouter = express.Router();
+
+app.use(express.static('public'));
+
+app.set('views','./src/views');
+app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+	.get(function(req, res){
+		res.send('Hello Books');
+	});
+
+bookRouter.route('/single')
+	.get(function(req, res){
+		res.send('Hello from Single Book');
+	});
+
+app.use('/Books', bookRouter);
+
+app.get('/', function(req, res) {
+    res.render('index', {
+    	title: 'Helo from render', 
+    	nav: [{
+	    		Link:'/Books', 
+	    		Text: 'Books'
+    	}, {
+	    		Link:'/Authors',
+	    		Text: 'Authors'
+	    	}]
+    	});
+});
+
+app.get('/books', function(req, res) {
+    res.send('Books');
+});
 
 app.listen(port, function(err) {
-	console.log('Server running on port' + port);
+    console.log('Server running on port ' + port);
 });
