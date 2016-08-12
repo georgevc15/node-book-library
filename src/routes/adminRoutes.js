88cadc648d943d822e12fa32143857602b1dfbc9
George Codat Tudoran <office@solcreation.ro>
2016-08-12 13:28:15 +0300

    Added admin routes

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var books = [
+		{
+				title: 'Books 1',
+				genre: 'Genre 1',
+				author: 'Author 1',
+				read: false
+		},
+		{
+				title: 'Books 2',
+				genre: 'Genre 2',
+				author: 'Author 2',
+				read: false
+		},
+		{
+				title: 'Books 3',
+				genre: 'Genre 3',
+				author: 'Author 3',
+				read: false
+		},
+		{
+				title: 'Books 4',
+				genre: 'Genre 4',
+				author: 'Author 4',
+				read: false
+		}
+			];
+
+var router = function (nav) {
+
+	adminRouter.route('/addBooks')
+
+				.get(function(req, res) {
+					var url = 'mongodb://localhost/libraryApp';
+					mongodb.connect(url, function(err, db) {
+						var collection = db.collection('books');
+						collection.insertMany(books, 
+							function(err, results){
+								res.send(results);
+								db.close();
+							});
+					});
+					//res.send('inserting books');
+				});
+
+	return adminRouter;
+};
+
+module.exports = router;
