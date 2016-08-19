4f60904d0e472aeb1cadbf3bbb38d19a30cf164e
George Codat Tudoran <office@solcreation.ro>
2016-08-19 19:52:23 +0300

    Started the implementation of passport

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 
@@ -15,14 +19,29 @@ var nav = [{
 
 var bookRouter = require('./src/routes/booksRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false })); // parse application/x-www-form-urlencoded 
+app.use(bodyParser.json()); // parse application/json 
+app.use(cookieParser());
+app.use(session({
+    secret: 'library',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+
+}));// se pune orice cuvant vreau ca si parola
+
+
+require('./src/config/passport')(app);
 
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
