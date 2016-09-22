4c575ac92a5ac7233816427a9fb0be0d4ea353d8
George Codat Tudoran <office@solcreation.ro>
2016-09-22 18:45:23 +0300

    Successfully implemented external API service for getting book image and description

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,9 +1,33 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({explicitArray: false});
+
 var goodreadsService = function () {
 
 	var getBookById = function(id, cb) { // cb = callback
-			cb(null, {
-				description: 'Our description'
-			});
+			
+			var options = {
+				host: 'www.goodreads.com',
+				path: '/book/show/656?format=xml&key=4bLsYiLR8PSwGPYRpvNUvw'
+			};
+
+			var callback = function(response) {
+				var str = '';
+
+				response.on('data', function(chunk) {
+					str += chunk;
+				});
+				response.on('end', function() {
+					console.log(str);
+					parser.parseString(str,
+						function(err, result){
+							cb(null, 
+								result.GoodreadsResponse.book);			
+						});
+					});
+				};
+
+			http.request(options, callback).end();
 	};
 
 	return {
